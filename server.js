9248fbb2b2040dc790755e65931a2669c9565f38
avdhantyagi <avdhantyagi1011@gmail.com>
2024-09-14 02:50:19 +0530

    Server Basic

---
+++ server.js
@@ -0,0 +1,18 @@
+const io = require("socket.io")(5174, {
+    cors: {
+      origin: "http://localhost:5173",
+      methods: ["GET", "POST"],
+    },
+}) 
+
+io.on("connection", socket => {
+  socket.on("get-document", documentId => {
+    const data = ""
+    socket.join(documentId)
+    socket.emit('load-document', data)
+
+    socket.on("send-changes", delta => {
+      socket.broadcast.to(documentId).emit("receive-changes", delta)
+    })
+  })
+})
